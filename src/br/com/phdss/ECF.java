6a6c8ed2b976540dc7323c88674f2fad930157ee
Pedro Henrique Lira <pedroh.lira@gmail.com>
2014-01-14 09:29:03 -0300

    Versão 1.04_1
    Correção da classe que instancia as ECFs.

--- src/br/com/phdss/ECF.java
+++ src/br/com/phdss/ECF.java
@@ -1,11 +1,5 @@
 package br.com.phdss;
 
-import br.com.phdss.fiscal.ACBR;
-import br.com.phdss.fiscal.Bematech;
-import br.com.phdss.fiscal.Daruma;
-import br.com.phdss.naofiscal.BematechNF;
-import br.com.phdss.naofiscal.DarumaNF;
-
 /**
  * Classe que abstrai a criação do objet que acessa o ECF para impressão.
  *
@@ -24,34 +18,14 @@ public class ECF {
     /**
      * Metodo que seta o objeto de ECF de acordo com o configurado.
      *
-     * @param tipo o texto que diz qual o tipo de ECF desejado.
+     * @param classe o texto que diz qual o tipo de ECF desejado.
      */
-    public static void setInstancia(String tipo) {
-        ECF.setInstancia(EECF.valueOf(tipo));
-    }
-
-    /**
-     * Metodo que seta o objeto de ECF de acordo com o configurado.
-     *
-     * @param tipo o enum que diz qual o tipo de ECF desejado.
-     */
-    public static void setInstancia(EECF tipo) {
-        switch (tipo) {
-            case BEMATECH:
-                ecf = new Bematech();
-                break;
-            case BEMATECH_NF:
-                ecf = new BematechNF();
-                break;
-            case DARUMA:
-                ecf = new Daruma();
-                break;
-            case DARUMA_NF:
-                ecf = new DarumaNF();
-                break;
-            default:
-                ecf = new ACBR();
-                break;
+    public static void setInstancia(String classe) {
+        try {
+            Class imp = Class.forName(classe);
+            ecf = (IECF) imp.newInstance();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+            ecf = null;
         }
     }
 
